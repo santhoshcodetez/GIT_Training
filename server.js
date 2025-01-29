d822f80e9292fb1e5688a63f536b971a8bea9549
Santhosh _codetez <santhosh@codetez.com>
2025-01-29 18:25:37 +0530

    Issue with log message

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express";
 import { listen } from '../api';
 const PORT=4000;
 listen(PORT,()=>{
-    console.log("Port runs in "+PORT+" Sucessfully");
+    conslogole.log("Port runs in "+PORT+" Sucessfully");
     console.log("Im Batman");
+    console.log("nan tha da leo leoo dasss");
     
 })

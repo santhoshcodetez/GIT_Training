84da332baafe4032fbf6ceba1f69c3c956e054c4
Santhosh _codetez <santhosh@codetez.com>
2025-01-29 18:44:32 +0530

    Added some logs

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ listen(PORT,()=>{
     conslogole.log("Port runs in "+PORT+" Sucessfully");
     console.log("Im Batman");
     console.log("nan tha da leo leoo dasss");
+    console.log("TIme is Gold");
+    
     
 })

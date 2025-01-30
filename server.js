c61fc60f45b23453559e32b5fc7c11c38880c9a9
Santhosh _codetez <santhosh@codetez.com>
2025-01-30 09:52:12 +0530

    Added Player

--- server.js
+++ server.js
@@ -1,8 +1,2 @@
-import express from "express";
-import { listen } from '../api';
-const PORT=4000;
-listen(PORT,()=>{
-    console.log("Port runs in "+PORT+" Sucessfully");
-    console.log("Im Batman");
-    
-})
+console.log("Cricket Players");
+
